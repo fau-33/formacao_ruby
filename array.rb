3d937315b34b24d24f14716b26a42f05b474ed12
Flávio Félix <fau_felix30@hotmail.com>
2025-10-03 18:26:32 -0300

    Start arrays

---
+++ array.rb
@@ -0,0 +1,20 @@
+# Criação: Crie um array chamado minha_lista com as tarefas iniciais: "Comprar café" e "Estudar arrays".
+
+minha_lista = ["Comprar café", "Estudar arrays"]
+
+# Adição/Início: Adicione uma nova tarefa, "Pagar contas", ao início da lista.
+
+minha_lista.unshift("Pagar contas")
+
+
+# Atualização: Altere o valor da segunda tarefa (que agora é "Comprar café") para "Comprar leite".
+
+minha_lista[1] = "Comprar leite"
+
+# Remoção/Início: Remova o primeiro item da lista (que é "Pagar contas"), simulando a conclusão da tarefa.
+
+minha_lista.shift
+
+# No final, seu código deve imprimir o array resultante.
+
+puts minha_lista
