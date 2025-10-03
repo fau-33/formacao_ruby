cd574c3579a63e70a0c5c1220963483e001bff9d
Flávio Félix <fau_felix30@hotmail.com>
2025-10-03 14:46:16 -0300

    Aulas estruturas de iteração

---
+++ for.rb
@@ -0,0 +1,9 @@
+fruits = ["Apple", "Banana", "Cherry"]
+
+fruit = "orange"
+
+for f in fruits
+  puts f
+end
+
+puts fruit
---
+++ times.rb
@@ -0,0 +1,9 @@
+4.times do
+  puts "Estou aprendendo times"
+end
+
+names = ["João", "Maria", "Pedro"]
+
+3.times do |index|
+  puts names[index]
+end
